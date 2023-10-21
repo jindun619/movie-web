a4c8fba4ce9cdbec60fdf2c2123802f4e936d2de
jindun619 <jindun619@naver.com>
2023-10-21 06:31:00 +0000

    2

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -13,9 +13,9 @@ const Seo = ({ title, og }: {
             <title>{`${title} | 조성민머리FLIX`}</title>
             <meta property="og:type" content="movie" />
             {/* <meta property="og:url" content="https://example.com/page.html"> */}
-            <meta property="og:title" content={og.title || "abc"} />
-            <meta property="og:image" content={og.image || "abc"} />
-            <meta property="og:description" content={og.description || "abc"} />
+            <meta property="og:title" content={"abc" || og.title} />
+            <meta property="og:image" content={"abc" || og.image} />
+            <meta property="og:description" content={"abc" || og.description} />
             <meta property="og:site_name" content="조성민머리FLIX" />
             <meta property="og:locale" content="ko_KR" />
         </Head>
