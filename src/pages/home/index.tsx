0767a48277a81052fdefdff4361780a97b1f2ed6
jindun619 <jindun619@naver.com>
2023-10-26 03:23:05 +0000

    1

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -8,34 +8,12 @@ import Seo from "@/components/Seo";
 
 import { MovieType } from "@/types";
 
-export default function HomePage() {
-  const [movie, setMovie] = useState<MovieType>();
-  // const [video, setVideo] = useState();
-
+export default function HomePage({ res1, res2 }: { res1: any[]; res2: any[] }) {
+  console.log(res1, res2);
   const setPage = useSetRecoilState(pageState);
 
   useEffect(() => {
     setPage(0);
-  }, []);
-
-  // Fetch Trending Movie Data
-  useEffect(() => {
-    axios
-      .get(`/api/trending/movie/day`, {
-        params: {
-          api_key: process.env.API_KEY,
-          region: "KR",
-          language: "ko-KR",
-          // page: 1,
-        },
-      })
-      .then((res) => {
-        console.log(res.data.results);
-        setMovie(res.data.results[0]);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
   }, []);
 
   const og = {
@@ -49,4 +27,39 @@ export default function HomePage() {
       <div>home</div>
     </>
   );
+}
+
+export async function getServerSideProps() {
+  const baseUrl = "https://api.themoviedb.org/3";
+
+  try {
+    //  Fetching trending movie by day
+    const res1 = await axios.get(`${baseUrl}/trending/movie/day`, {
+      params: {
+        api_key: process.env.API_KEY,
+        region: "KR",
+        language: "ko-KR",
+      },
+    });
+    const movieId = res1.data.movie.results[0].id;
+
+    //  Fetching movie details data
+    const res2 = await axios.get(`${baseUrl}/movie/${movieId}`, {
+      params: {
+        api_key: process.env.API_KEY,
+        region: "KR",
+        language: "ko-KR",
+      },
+    });
+    const movie = res2.data;
+    return {
+      props: { res1: res1.data.results, res2: res2.data.results },
+    };
+  } catch (error) {
+    return {
+      props: {
+        error: error,
+      },
+    };
+  }
 }
